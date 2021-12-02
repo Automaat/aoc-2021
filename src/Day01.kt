695c9d7b6c550c43e95a5e1ef23ab5109f6ea29c
Marcin Skalski <marcin.skalski@allegro.pl>
2021-12-02 06:15:12 +0100

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var lastDepth = 1000000
+        var result = 0
+        for (i in input) {
+            val actualDepth = i.toInt()
+            if (actualDepth > lastDepth) {
+                result++
+            }
+            lastDepth = actualDepth
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var lastDepth = 1000000
+        var result = 0
+        for (i in 0..input.size) {
+            if (i < input.size - 2) {
+                val depth = input[i].toInt()
+                val depth1 = input[i+1].toInt()
+                val depth2 = input[i+2].toInt()
+
+                val window = depth + depth1 + depth2
+                if (window > lastDepth) {
+                    result++
+                }
+                lastDepth = window
+            }
+        }
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
